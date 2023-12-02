79e02a0b7b3e8bc6b0a57e9a96e2844502e70eaf
gpavan123789 <106580961+gpavan123789@users.noreply.github.com>
2023-12-02 18:57:08 -0500

    update naming for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './Components/Header/Header';
 import LoginPage from './Components/Login/Login'; // Import your Login page component
 import SignupPage from './Components/SignUp/SignUp'; // Import your Signup page component
@@ -7,33 +7,16 @@ import HomePage from './Components/Home/Home';
 import './App.css';
 
 import LogoutPage from './Components/Logout/Logout';
-import SimpleForm from './components1/SimpleForm';
-import TemplatePage from './components2/TemplatePage';
+import SimpleForm from './UserDetails/SimpleForm';
+import TemplatePage from './Template_Page/TemplatePage';
 
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header />
-//         <Routes>
-//           <Route path="/login" element={<LoginPage />} />
-//           <Route path="/signup" element={<SignupPage />} />
-//           <Route path="/" element={<LoginPage />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
 
 function App() {
+  // const showHeader = location.pathname === '/' || location.pathname === '/login';
   return (
     <Router>
       <div className="App">
-     
-        {/* <LogoutPage /> */}
-
-        <Header />
+       <Header />
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
