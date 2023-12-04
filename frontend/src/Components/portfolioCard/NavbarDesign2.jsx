f43d00afdbcebf09c3f717763e56721c7e8b004b
gpavan123789 <106580961+gpavan123789@users.noreply.github.com>
2023-12-04 00:10:25 -0500

    update navbar

--- frontend/src/Components/portfolioCard/NavbarDesign2.jsx
+++ frontend/src/Components/portfolioCard/NavbarDesign2.jsx
@@ -20,9 +20,6 @@ const NavbarDesign2 = ({
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
       <a className="navbar-brand js-scroll-trigger" href="#page-top">
         <span className="d-block d-lg-none">{FullName}</span>
-        <span className="d-none d-lg-block">
-          <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={Thubmnail} alt={FullName} />
-        </span>
       </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
